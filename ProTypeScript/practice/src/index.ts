c2d0ef35b957e6758c960f73806d5a704baa2419
daikichidaze <dkch.yoshioka.t@gmail.com>
2022-05-21 13:15:35 +0700

    added samples

--- ProTypeScript/practice/src/index.ts
+++ ProTypeScript/practice/src/index.ts
@@ -1,2 +1,10 @@
 const message: string = "Hello world!";
 console.log(message);
+
+const val = null;
+const val2 = undefined;
+
+console.log(val, val2);
+
+// 2.3.5
+console.log(`123 + 456 = ${123 + 456}`);
