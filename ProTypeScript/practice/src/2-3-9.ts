af0106f93846d38b075fa5f5748437660ca80547
daikichidaze <dkch.yoshioka.t@gmail.com>
2022-05-21 13:16:55 +0700

    update for chap 2.3.9

---
+++ ProTypeScript/practice/src/2-3-9.ts
@@ -0,0 +1,11 @@
+import { createInterface } from "readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Input test: ", (line) => {
+  console.log(`${line} was inputed`);
+  rl.close();
+});
